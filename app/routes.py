b34e85e875056bb9b91da6813e218ed3fec481f9
Vladislav Nekto <vladislav.nekto.dev@gmail.com>
2020-04-09 16:52:40 +0600

    Добавление aboutme

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, request
 
 from app import app
 
@@ -6,12 +6,29 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-	return render_template("index.html", title="Index Page")
+    return render_template("index.html", title="Index Page")
+
 
 @app.route('/kirill')
 def kirill():
-	return render_template("specialpersons/kirill.html", title="Главный пидорас")
+    return render_template(
+        "specialpersons/kirill.html",
+        title="Главный пидорас"
+    )
+
+
 @app.route('/yahenni')
 def yahenni():
-	return render_template("specialpersons/yahenni.html", title="Якхенни")
+    return render_template("specialpersons/yahenni.html", title="Якхенни")
 
+
+@app.route('/aboutme')
+def aboutme():
+    useragent = request.headers.get('User-Agent')
+    ip = request.environ.get('HTTP_X_REAL_IP', request.remote_addr)
+    return render_template(
+        "aboutme.html",
+        title="Большой брат",
+        useragent=useragent,
+        ip=ip
+    )
