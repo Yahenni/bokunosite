44dd6ad8f4a6f121c38b64bf2e3adee65a82f691
Vladislav Nekto <vladislav.nekto.dev@gmail.com>
2020-05-18 18:14:44 +0600

    Мелкое исправление. Version: 0.0.1

--- config.py
+++ config.py
@@ -18,6 +18,7 @@ class Config:
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USE_SSL = os.environ.get('MAIL_USE_SSL') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     if os.environ.get('ADMINS'):
