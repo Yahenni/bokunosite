0f2ad6b96b44b1ce6fee27cd9061a3fa112c47ac
Vladislav Nekto <vladislav.nekto.dev@gmail.com>
2020-05-18 18:29:11 +0600

    Небольшой баг фикс. Version: 0.0.1

--- config.py
+++ config.py
@@ -21,5 +21,6 @@ class Config:
     MAIL_USE_SSL = os.environ.get('MAIL_USE_SSL') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    SECURITY_EMAIL_SENDER = os.environ.get('SECURITY_EMAIL_SENDER') is not None
     if os.environ.get('ADMINS'):
         ADMINS = os.environ.get('ADMINS').split(';')
