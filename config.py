30884700cb059645a573ef6396051638ab66adb4
Vladislav Nekto <vladislav.nekto.dev@gmail.com>
2020-05-19 18:38:40 +0600

    Version: 0.0.3 | Добавлена поддержка файла конфигурации .env

--- config.py
+++ config.py
@@ -1,6 +1,9 @@
 import os
+from dotenv import load_dotenv
 
-basedir = os.path.abspath(os.path.dirname(__name__))
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
+
 
 class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
